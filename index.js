a0a02c1ac469b35aa0b7e23a8a8a1f69199ffb35
Jake Samuels <samuels242@googlemail.com>
2022-07-14 13:59:57 +0100

    Find the Longest Word in a String

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ function factorialize(num) {
   //   }
   return num < 0 ? -1 : num == 0 ? 1 : num * factorialize(num - 1);
 }
+
+// Find the Longest Word in a String
+function findLongestWordLength(str) {
+  let count = 0;
+  str.split(" ").forEach((word) => {
+    if (word.length > count) {
+      count = word.length;
+    }
+  });
+  return count;
+}
