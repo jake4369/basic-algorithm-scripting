ed39f724add5e39dc15f0170c134d826d38fbfa4
Jake Samuels <samuels242@googlemail.com>
2022-07-14 19:14:53 +0100

    Slice and Splice

--- index.js
+++ index.js
@@ -78,3 +78,13 @@ function titleCase(str) {
     .map((word) => word[0].toUpperCase() + word.slice(1))
     .join(" ");
 }
+
+// Slice and Splice
+function frankenSplice(arr1, arr2, n) {
+  const arr2Copy = arr2.slice();
+  for (let i = 0; i < arr1.length; i++) {
+    arr2Copy.splice(n, 0, arr1[i]);
+    n++;
+  }
+  return arr2Copy;
+}
