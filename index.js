8b5ec2496238b10cf09f45902087107f1e8f4fb5
Jake Samuels <samuels242@googlemail.com>
2022-07-15 14:22:33 +0100

    Chunky Monkey

--- index.js
+++ index.js
@@ -108,3 +108,12 @@ function mutation(arr) {
   }
   return true;
 }
+
+// Chunky Monkey
+function chunkArrayInGroups(arr, size) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += size) {
+    newArr.push(arr.slice(i, i + size));
+  }
+  return newArr;
+}
